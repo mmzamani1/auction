0edc506a60a9e35f855c159a106c76ad1d5058bb
mmzamani1 <zamanimohammad582@gmail.com>
2024-10-09 18:31:23 +0330

    "Updated auction app: modified templates, views, and URLs; removed gallery view and template; updated requirements.txt"

--- auction/urls.py
+++ auction/urls.py
@@ -2,11 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("home", views.index, views.template, name="index"),
+    path("home", views.index, name="index"),
     path("login", views.login_page, name="login"),
     path("", views.logout_view, name="logout"),
     path("signup", views.signup_view, name="signup"),
     path("post-Item", views.post_item, name="postitem"),
     path("about", views.about_page, name="about"),
-    path("gallery", views.gallery, name="gallery"),
 ]
--- auction/views.py
+++ auction/views.py
@@ -27,7 +27,7 @@ def template(request):
 def index(request):
     
     """response = requests.get('https://fakestoreapi.com/products')
-    for i in range(3, 5):
+    for i in range(5, 8):
         
         title = response.json()[i]['title']
         starting_bid = response.json()[i]['price']
@@ -48,27 +48,36 @@ def index(request):
     
         
     items = Item.objects.all()
+    category_list =  Categorylist.objects.all()
     
     return render(request, f'{base_url}/index.html', {
         'items': items,
+        'category_list': category_list,
     })
 
 def login_page(request):
+    category_list =  Categorylist.objects.all()
+    
     if request.method == 'POST':
         username = request.POST['Username']
         password = request.POST['Password']
         
         user = authenticate(request, username=username, password=password)
         
+        
         if user is not None:
             login(request, user)
             return HttpResponseRedirect(reverse("index"))
             
         else:
-            return render(request, f'{base_url}/login.html')
+            return render(request, f'{base_url}/login.html', {
+                'category_list': category_list,
+            })
         
     else:
-        return render(request, f'{base_url}/login.html')
+        return render(request, f'{base_url}/login.html', {
+            'category_list': category_list,
+        })
 
 @login_required
 def logout_view(request):
@@ -76,6 +85,9 @@ def logout_view(request):
     return HttpResponseRedirect(reverse("login"))
 
 def signup_view(request):
+    
+    category_list =  Categorylist.objects.all()
+    
     if request.method == 'POST':
         username = request.POST['Username']
         email = request.POST['Email']
@@ -83,6 +95,7 @@ def signup_view(request):
         password2 = request.POST['Password2']
         fname = request.POST['Firstname']
         lname = request.POST['Lastname']
+        
 
         if password == password2:
             user = User.objects.create_user(username=username, password=password, email=email)
@@ -101,7 +114,9 @@ def signup_view(request):
             return render(request, f'{base_url}/signup.html')
     
     else:
-        return render(request, f'{base_url}/signup.html')
+        return render(request, f'{base_url}/signup.html',{
+            'category_list': category_list,
+        })
     
 def post_item(request):
     if request.method == 'POST':
@@ -131,7 +146,9 @@ def post_item(request):
         })
 
 def about_page(request):
-    return render(request, f'{base_url}/about.html')
-
-def gallery(request):
-    return render(request, f'{base_url}/gallery.html')
+    
+    category_list =  Categorylist.objects.all()
+    
+    return render(request, f'{base_url}/about.html',{
+        'category_list': category_list,
+    })
