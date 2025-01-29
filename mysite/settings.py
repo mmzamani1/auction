100bcd0cf61a5748dd0b75335a0ad47a6db184a6
mmzamani1 <zamanimohammad582@gmail.com>
2025-01-29 11:10:52 +0330

    `Update Dockerfile and remove unused app dependencies`

--- mysite/settings.py
+++ mysite/settings.py
@@ -33,7 +33,6 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'auction',
     'weatherApp',
-    'bootstrap5',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
