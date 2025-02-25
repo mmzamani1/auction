4a240534f554cbd687aaf8721f563e0ceff11643
mmzamani1 <zamanimohammad582@gmail.com>
2025-02-25 10:26:36 +0330

    `Updated URLs in auction app and main project to include trailing slashes`

--- mysite/urls.py
+++ mysite/urls.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path("auction/", include("auction.urls")),
-    path("weather/", include("weatherApp.urls")),
+    path("", include("auction.urls")),
     path('admin/', admin.site.urls),
 ]
